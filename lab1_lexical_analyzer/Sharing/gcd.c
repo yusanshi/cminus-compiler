2fd29141fea3c8800a15ed5217d4578e93b761d5
PB17051053 <676740040@qq.com>
2019-10-11 17:40:58 +0800

    init lab2

--- lab1_lexical_analyzer/Sharing/gcd.c
+++ lab1_lexical_analyzer/Sharing/gcd.c
@@ -1,17 +1,21 @@
-#include<stdio.h>
-int gcd (int u, int v) { /* calculate the gcd of u and v */
-    if (v == 0) return u;
-    else return gcd(v, u - u / v * v); /* v,u-u/v*v is equals to u mod v*/
+#include <stdio.h>
+int gcd(int u, int v) { /* calculate the gcd of u and v */
+    if (v == 0)
+        return u;
+    else
+        return gcd(v, u - u / v * v); /* v,u-u/v*v is equals to u mod v*/
 }
 int main() {
-    int x; int y; int temp;
+    int x;
+    int y;
+    int temp;
     x = 72;
     y = 18;
-    if (x<y) {
+    if (x < y) {
         temp = x;
         x = y;
         y = temp;
     }
-    printf("%d\n", gcd(x,y));
+    printf("%d\n", gcd(x, y));
     return 0;
 }
