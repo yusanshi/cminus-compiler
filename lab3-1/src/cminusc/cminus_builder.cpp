e02feccd6ef4d8b0759e3c0b788fb38bc045fc10
Wang Ruochen <wangkevin920@163.com>
2019-11-08 17:54:28 +0800

    Setup working environment
    - Ignore testcase directory
    - Add .clang-format
    - Enable `-Wall`

--- lab3-1/src/cminusc/cminus_builder.cpp
+++ lab3-1/src/cminusc/cminus_builder.cpp
@@ -1,37 +1,81 @@
 #include "cminus_builder.hpp"
-#include <iostream>
+
+using namespace llvm;
+using namespace std;
 
 // You can define global variables here
 // to store state
 
-void CminusBuilder::visit(syntax_program &node) {}
+void CminusBuilder::visit(syntax_program &node) {
+    //
+    for (auto d : node.declarations) {
+        d->accept(*this);
+    }
+}
 
-void CminusBuilder::visit(syntax_num &node) {}
+void CminusBuilder::visit(syntax_num &node) {
+    //
+}
 
-void CminusBuilder::visit(syntax_var_declaration &node) {}
+void CminusBuilder::visit(syntax_var_declaration &node) {
+    //
+}
 
-void CminusBuilder::visit(syntax_fun_declaration &node) {}
+void CminusBuilder::visit(syntax_fun_declaration &node) {
+    auto type = (node.type == TYPE_INT) ? Type::getInt32Ty(this->context)
+                                        : Type::getVoidTy(this->context);
 
-void CminusBuilder::visit(syntax_param &node) {}
+    auto func =
+        Function::Create(FunctionType::get(type, /* TODO: params ,*/ false),
+                         GlobalValue::LinkageTypes::ExternalLinkage,
+                         node.id.c_str(), this->module.get());
+    this->scope.push(node.id, func);
+}
 
-void CminusBuilder::visit(syntax_compound_stmt &node) {}
+void CminusBuilder::visit(syntax_param &node) {
+    //
+}
 
-void CminusBuilder::visit(syntax_expresion_stmt &node) {}
+void CminusBuilder::visit(syntax_compound_stmt &node) {
+    //
+}
 
-void CminusBuilder::visit(syntax_selection_stmt &node) {}
+void CminusBuilder::visit(syntax_expresion_stmt &node) {
+    //
+}
 
-void CminusBuilder::visit(syntax_iteration_stmt &node) {}
+void CminusBuilder::visit(syntax_selection_stmt &node) {
+    //
+}
 
-void CminusBuilder::visit(syntax_return_stmt &node) {}
+void CminusBuilder::visit(syntax_iteration_stmt &node) {
+    //
+}
 
-void CminusBuilder::visit(syntax_var &node) {}
+void CminusBuilder::visit(syntax_return_stmt &node) {
+    //
+}
 
-void CminusBuilder::visit(syntax_assign_expression &node) {}
+void CminusBuilder::visit(syntax_var &node) {
+    //
+}
 
-void CminusBuilder::visit(syntax_simple_expression &node) {}
+void CminusBuilder::visit(syntax_assign_expression &node) {
+    //
+}
 
-void CminusBuilder::visit(syntax_additive_expression &node) {}
+void CminusBuilder::visit(syntax_simple_expression &node) {
+    //
+}
 
-void CminusBuilder::visit(syntax_term &node) {}
+void CminusBuilder::visit(syntax_additive_expression &node) {
+    //
+}
 
-void CminusBuilder::visit(syntax_call &node) {}
+void CminusBuilder::visit(syntax_term &node) {
+    //
+}
+
+void CminusBuilder::visit(syntax_call &node) {
+    //
+}
